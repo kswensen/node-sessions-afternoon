e6ba32eb1fb3bb10f38594592c31132fc28357ea
StayinNasty <keganswensen@gmail.com>
2017-08-29 15:14:27 -0600

    finished

---
+++ server/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+const checkForSession = require('./middlewares/checkForSession');
+
+const swag_controller = require('./controllers/swag_controller');
+const auth_controller = require('./controllers/auth_controller');
+const cart_controller = require('./controllers/cart_controller');
+const search_controller = require('./controllers/search_controller');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(session({
+    secret: "IDontHaveOne",
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(checkForSession);
+
+app.get('/api/swag', swag_controller.read);
+
+app.post('/api/login', auth_controller.login);
+app.post('/api/register', auth_controller.register);
+app.post('/api/signout', auth_controller.signout);
+app.get('/api/user', auth_controller.getUser);
+
+app.post('/api/cart', cart_controller.add);
+app.post('/api/cart/checkout', cart_controller.checkout);
+app.delete('/api/cart', cart_controller.delete);
+
+app.get('/api/search', search_controller.search);
+
+const port = 3000;
+app.listen(port, console.log(`It's lit on ${port} fam!`));
